45914ad4f2c67f44e35fb2ff544f57e52d98a013
Luca Arnold <luca@lxuca.com>
2024-08-21 13:52:46 +0200

    Test

---
+++ src/Core/Shared/Utilities/SoundFX.lua
@@ -0,0 +1,30 @@
+--[=[
+    @class SoundFX
+    A simplistic way to store and play sound effects
+    Sounds must be stored in ReplicatedStorage inside a folder called "SoundFXs"
+    You can (and advised) to store sounds in subfolders if you have a large amount
+]=]
+
+local SoundFX = {}
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Sounds = {}
+
+function SoundFX:Init()
+    local soundFolder: Folder? = ReplicatedStorage:FindFirstChild("SoundFXs")
+
+    if soundFolder ~= nil then
+        for _, asset in pairs(soundFolder:GetDescendants()) do
+            if asset:IsA("Sound") then
+                if Sounds[asset.Name] ~= nil then
+                    warn(`[SoundFX] Duplicate sound with name '{asset.Name}' sound. Ignored.`)
+                else
+                    Sounds[asset.Name] = asset
+                end
+            end
+        end
+    end
+end
+
+return SoundFX
